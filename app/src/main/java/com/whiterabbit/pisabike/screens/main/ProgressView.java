8c356d7e1d930c6921f9cd0884ef517f44ec413b
Federico Paolinelli <fedepaol@gmail.com>
2016-10-10 21:47:48 +0200

    Refactored progress view

--- app/src/main/java/com/whiterabbit/pisabike/screens/main/ProgressView.java
+++ app/src/main/java/com/whiterabbit/pisabike/screens/main/ProgressView.java
@@ -2,7 +2,6 @@ package com.whiterabbit.pisabike.screens.main;
 
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
-import android.app.Activity;
 import android.content.Context;
 import android.os.Handler;
 import android.util.AttributeSet;
@@ -10,10 +9,8 @@ import android.view.LayoutInflater;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.ViewAnimator;
 
 import com.whiterabbit.pisabike.R;
-
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -33,6 +30,11 @@ public class ProgressView extends LinearLayout {
     @Bind(R.id.update_view_message)
     TextView mMessage;
 
+    @Bind(R.id.update_view_icon_error)
+    ImageView mIconError;
+
+    private final Handler mHandler;
+
     public ProgressView(Context context, AttributeSet attrs) {
         super(context, attrs);
         LayoutInflater inflater = (LayoutInflater) context
@@ -40,6 +42,7 @@ public class ProgressView extends LinearLayout {
         inflater.inflate(R.layout.update_view_layout, this, true);
         ButterKnife.bind(this);
         setVisibility(GONE);
+        mHandler = new Handler();
     }
 
 
@@ -52,6 +55,7 @@ public class ProgressView extends LinearLayout {
         mMessage.setText(message);
         mIcon.setVisibility(VISIBLE);
         mIconDone.setVisibility(GONE);
+        mIconError.setVisibility(GONE);
         ValueAnimator rotation = ObjectAnimator.ofFloat(mIcon, "rotation", 0, 180);
         rotation.setRepeatCount(ValueAnimator.INFINITE);
         rotation.setDuration(800);
@@ -59,31 +63,26 @@ public class ProgressView extends LinearLayout {
         animate().scaleX(1).scaleY(1).setDuration(600);
     }
 
-    public void setDone(String message) {
-        Handler handler = new Handler();
-        handler.postDelayed(() -> {
+    private void setProgressFinished(String message, boolean success) {
+        mHandler.postDelayed(() -> {
             mIsProgress = false;
             mMessage.setText(message);
             mIcon.setVisibility(GONE);
-            mIconDone.setVisibility(VISIBLE);
-
+            if (success) {
+                mIconDone.setVisibility(VISIBLE);
+            } else {
+                mIconError.setVisibility(VISIBLE);
+            }
             new Handler().postDelayed( () -> {
                 animate().scaleX(0).scaleY(0).setDuration(600);}, 1000);
         }, 1000);
     }
 
-    public void setError(String error) {
-        mIsProgress = false;
-        mIcon.setImageResource(R.drawable.ic_done_white_24px);
-        mMessage.setText(error);
-        mIcon.setVisibility(GONE);
-        mIconDone.setVisibility(VISIBLE);
-
-        Handler handler = new Handler();
-        handler.postDelayed(() -> {
-            animate().scaleX(0).scaleY(0).setDuration(600);
-        }, 1000);
+    public void setDone(String message) {
+        setProgressFinished(message, true);
     }
 
-
+    public void setError(String error) {
+        setProgressFinished(error, false);
+    }
 }
