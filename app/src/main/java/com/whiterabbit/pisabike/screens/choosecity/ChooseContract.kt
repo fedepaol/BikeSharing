946e762b6e78f63923840c958cf724c664e99589
Federico Paolinelli <fedepaol@gmail.com>
2017-09-17 15:24:18 +0200

    Started working with room

---
+++ app/src/main/java/com/whiterabbit/pisabike/screens/choosecity/ChooseContract.kt
@@ -0,0 +1,20 @@
+package com.whiterabbit.pisabike.screens.choosecity
+
+import android.support.annotation.NonNull
+
+
+
+/**
+ * Created by fedepaol on 20/07/17.
+ */
+
+interface ChooseContract {
+
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+}
